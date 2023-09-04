33859cc5b6b15187640032cce6474789905dd389
Nilson Melo <nilson.melo@sti.ufc.br>
2023-09-04 18:32:18 -0300

    feature: arquitetura base - entidade e enum

---
+++ src/main/java/br/com/treinaweb/twclientes/model/Pessoa.java
@@ -0,0 +1,85 @@
+package br.com.treinaweb.twclientes.model;
+
+import java.time.LocalDate;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.OneToOne;
+
+@MappedSuperclass
+public abstract class Pessoa extends Entidade{
+
+    @Column(nullable = false, length = 80)
+    private String nome;
+
+    @Column(nullable = false, length = 14, unique = true)
+    private String cpf;
+
+    @Column(nullable = false, length = 15)
+    private String telefone;
+
+    @Column(nullable = false, length = 80, unique = true)
+    private String email;
+
+    @Column(nullable = false, name = "data_nascimento")
+    @DateTimeFormat(iso = ISO.DATE)
+    private LocalDate dataNascimento;
+
+    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "endereco_id_fk", nullable = false)
+    private Endereco endereco;
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(LocalDate dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+}
